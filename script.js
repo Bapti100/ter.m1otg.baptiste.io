4aa32736264c190dfa3484f6ee2933d1f03cc78b
Bapti100 <baptiste.de.livry@gmail.com>
2026-09-23 22:26:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+// Réservé pour de futures interactions (menu mobile, animations au scroll, etc.).
+// Rien n'est nécessaire pour le moment : le défilement fluide est géré en CSS
+// (scroll-behavior) et le nuage de points a son propre script dans visu-nuage.html.
